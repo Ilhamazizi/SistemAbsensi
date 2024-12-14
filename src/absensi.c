c1f86eae191863d049f9551f143ebab251233786
Muhammad I.A <rudolph@debian.org>
2024-12-14 07:52:56 +0100

    add function recordAttendance

--- src/absensi.c
+++ src/absensi.c
@@ -26,3 +26,42 @@ void addEmployee(Employee employees[], int *totalEmployees) {
 
 	printf("Karyawan berhasil ditambahkan!\n");
 }
+
+/* fungsi kehadiran */
+void recordAttendance(Employee employees[], int totalEmployees) {
+	int id, choice;
+	printf("Masukkan ID karyawan: ");
+	scanf("%d", &id);
+
+	for (int i = 0; i < totalEmployees; i++) {
+		if (employees[i].id == id) {
+			printf("Pilih status kehadiran untuk %s:\n", employees[i].name);
+			printf("1. Hadir\n2. Izin\n3. Sakit\n4. Alpha\n");
+			printf("Pilihan: ");
+			scanf("%d", &choice);
+
+			switch (choice) {
+				case 1:
+					employees[i].hadir++;
+					break;
+				case 2:
+					employees[i].izin++;
+					break;
+				case 3:
+					employees[i].sakit++;
+					break;
+				case 4:
+					employees[i].alpha++;
+					break;
+				default:
+					printf("Pilihan tidak valid!\n");
+					return;
+			}
+
+			printf("Absensi berhasil direcord!\n");
+			return;
+		}
+	}
+
+	printf("Karyawan dengan ID %d tidak ditemukan!\n", id);
+}
