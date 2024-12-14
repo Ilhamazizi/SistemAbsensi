af3d5197f90b9f2d66531a98c35ea2c61a9311bc
Muhammad I.A <rudolph@debian.org>
2024-12-14 08:43:41 +0100

    main choice

--- main.c
+++ main.c
@@ -6,6 +6,40 @@ int main() {
 	int totalEmployees = 0;
 	int choice;
 	Employee employees[MAX_EMPLOYEES];	
+
+	do {
+		printf("\nSistem Absensi Karyawan\n");
+		printf("1. Tambah Karayawan\n");
+		printf("2. Record Kehadiran\n");
+		printf("3. Tampilkan Laporan");
+		printf("Keluar\n");
+
+		printf("Pilihan: ");
+		scanf("%d", choice);
+
+		switch (choice) {
+			case 1:
+				addEmployee(employees, &totalEmployees);
+				break;
+			case 2:
+				recordAttendance(employees, totalEmployees);
+				break;
+			case 3:
+				showAttendanceRecord(employees, totalEmployees);
+				break;
+			case 4:
+				printf("Keluar dari program.\n");
+				break;
+			default:
+				printf("Pilihan tidak valid!\n");
+
+		}
+	}
+
+	while (choice != 4);
+	return 0;
 }
+
+
 
 
